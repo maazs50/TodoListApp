66e154b0c2fcca4213923ecfe7152c4bb27bfb1f
apple <apple@apples-MacBook-Pro.local>
2020-12-03 19:28:11 +0530

    ListAdapter view binding

--- app/src/main/java/com/mvvm/todo/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/mvvm/todo/ui/tasks/TasksFragment.kt
@@ -1,12 +1,34 @@
 package com.mvvm.todo.ui.tasks
 
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mvvm.todo.R
+import com.mvvm.todo.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TasksFragment:Fragment(R.layout.fragment_tasks){
     private val viewModel: TasksViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentTasksBinding.bind(view)
+        val taskAdapter = TasksAdapter()
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+
+            }
+        }
+       viewModel.tasks.observe(viewLifecycleOwner){
+           taskAdapter.submitList(it)
+       }
+    }
 }
--- app/src/main/java/com/mvvm/todo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/mvvm/todo/ui/tasks/TasksViewModel.kt
@@ -2,10 +2,12 @@ package com.mvvm.todo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.mvvm.todo.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ): ViewModel() {
+        val tasks = taskDao.getTasks().asLiveData()
 
 }
